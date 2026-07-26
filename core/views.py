cabfa26c2e60d01a75713e0a4b2c3beccce3fc51
musafir1banjaara <kunal.work786@gmail.com>
2026-07-26 15:03:26 +0530

    Data added to database via html forms ui

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Job
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -7,4 +8,13 @@ def home(request):
     return render(request, "index.html", {"jobs":jobs})
 
 def create_job(request):
+    if request.method == "POST":
+        Job.objects.create(
+            title=request.POST.get("title"),
+            description=request.POST.get("description"),
+            company_name=request.POST.get("company_name"),
+            location=request.POST.get("location"),
+            salary=request.POST.get("salary"),
+        )
+        return redirect("/")
     return render(request, "addform.html")
