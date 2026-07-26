2e915bfd8aeaf716600b0c89ae220944c85afb49
musafir1banjaara <kunal.work786@gmail.com>
2026-07-26 12:39:47 +0530

    Job table created using models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    company_name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.company_name} {self.title}"
+    # return self.title + ":" + self.company_name
+
